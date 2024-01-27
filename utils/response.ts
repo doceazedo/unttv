fad92b21ec435dd80db0128b6f273293ef1fe0cd
doceazedo <me@doceazedo.com>
2024-01-26 23:42:11 -0300

    Update badges response

--- utils/response.ts
+++ utils/response.ts
@@ -4,10 +4,13 @@ export const formatBadgesResponse = (badges: HelixChatBadgeSet[]) =>
   badges.map((badge) => ({
     id: badge.id,
     versions: badge.versions.map((version) => ({
-      clickAction: version.clickAction,
-      clickUrl: version.clickUrl,
-      description: version.description,
       id: version.id,
       title: version.title,
+      description: version.description,
+      clickAction: version.clickAction,
+      clickUrl: version.clickUrl,
+      image_url_1x: version.getImageUrl(1),
+      image_url_2x: version.getImageUrl(2),
+      image_url_4x: version.getImageUrl(4),
     })),
   }));
