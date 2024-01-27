d7ca616b62512cc6251de2cb0926458cb0a958dd
doceazedo <me@doceazedo.com>
2024-01-26 22:41:09 -0300

    Update route rules

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,11 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
+  routeRules: {
+    "/**": {
+      cache: process.env.NODE_ENV === "production" ? { maxAge: 60 } : undefined,
+      cors: true,
+    },
+  },
   experimental: {
     openAPI: true,
   },
