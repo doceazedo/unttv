c06a9fecc1a495159d979402da0928d7c81485e4
doceazedo <me@doceazedo.com>
2024-01-26 22:48:50 -0300

    Add Vercel preset

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,6 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
+  preset: "vercel",
   routeRules: {
     "/**": {
       cache: process.env.NODE_ENV === "production" ? { maxAge: 60 } : undefined,
